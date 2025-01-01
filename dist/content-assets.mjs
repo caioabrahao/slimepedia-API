b4ebcebb57a52228cbb0d66777bbaa027c8a7607
caioabrahao <contato.caioabrahao@gmail.com>
2025-01-01 16:29:15 -0300

    Major Vercel Routing Fix. Entry point defined

--- dist/content-assets.mjs
+++
@@ -1,4 +0,0 @@
-
-import __ASTRO_IMAGE_IMPORT_ZVfUS6 from "../../assets/Glitch_Slime.webp?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx";
-export default new Map([["../../assets/Glitch_Slime.webp?astroContentImageFlag=&importer=src%2Fcontent%2Fdocs%2Findex.mdx", __ASTRO_IMAGE_IMPORT_ZVfUS6]]);
-		
--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -4,33 +4,31 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	
-	integrations: [
-		starlight({
-			title: 'Slimepedia API',
-			social: {
-				github: 'https://github.com/caioabrahao/slimepedia-API',
-			},
-			sidebar: [
-				{
-					label: 'Introduction',
-					items: [
-						{ label: 'Overview', slug: 'introduction/overview' },
-					],
-				},
-				{
-					label: 'Guides',
-					
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Getting Started', slug: 'guides/getting-started' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  output: 'static',
+  integrations: [
+    starlight({
+      title: 'Slimepedia API',
+      social: {
+        github: 'https://github.com/caioabrahao/slimepedia-API',
+      },
+      sidebar: [
+        {
+          label: 'Introduction',
+          items: [
+            { label: 'Overview', slug: 'introduction/overview' },
+          ],
+        },
+        {
+          label: 'Guides',
+          items: [
+            { label: 'Getting Started', slug: 'guides/getting-started' },
+          ],
+        },
+        {
+          label: 'Reference',
+          autogenerate: { directory: 'reference' },
+        },
+      ],
+    }),
+  ],
 });
