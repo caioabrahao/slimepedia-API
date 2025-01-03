4dd865090bc98129778741870b4e2b19624fdb8e
Caio Abrahão <contato.caioabrahao@gmail.com>
2025-01-03 00:11:38 -0300

    Enhanced API Routing for Plorts and Slimes

--- api/src/routes.js
+++ api/src/routes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const slimes = require('../data/slimes.json');
 const plorts = require('../data/plorts.json');
 
-// Slime routes
+// 🥚 Slime routes ----------------
 router.get('/slimes/count', (req, res) => {
   res.json({ count: slimes.count });
 });
@@ -13,9 +13,13 @@ router.get('/slimes', (req, res) => {
   res.json(slimes);
 });
 
+router.get('/slime', (req, res) => { 
+  res.redirect('slimes');   // Redirect 'slime' to 'slimes' (typo safety net)
+});
+
 router.get('/slimes/:id', (req, res) => {
   const slimeId = parseInt(req.params.id, 10); // Convert ID to number
-  const slime = slimes.slimes.find(s => s.id === slimeId);
+  const slime = slimes.results.find(s => s.id === slimeId);
   
   if (slime) {
     res.json(slime);
@@ -24,10 +28,25 @@ router.get('/slimes/:id', (req, res) => {
   }
 });
 
-// Plort routes
-
+// 💎 Plort routes ----------------
 router.get('/plorts', (req, res) => {
   res.json(plorts);
 });
+
+router.get('/plorts/:id', (req, res) => {
+  const plortId = parseInt(req.params.id, 10); // Convert ID to number
+  const plort = plorts.plorts.find(s => s.id === plortId);
+  
+  if (plort) {
+    res.json(plort);
+  } else {
+    res.status(404).json({ error: 'Plort Data not found' });
+  }
+});
+
+
+
+
+
 
 module.exports = router;
